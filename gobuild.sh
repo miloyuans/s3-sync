edb6ae6d3956cd4a3c87c61980fc4d6fc91f5d0d
milo <miloyuans@gmail.com>
2025-09-10 10:59:17 +0400

    all build sh

---
+++ gobuild.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+Packages=$1
+# Build for x86 (32-bit)
+GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o ${Packages}-linux-386_32 main.go
+echo "Built monitor-service-386 for x86"
+
+# Build for AMD64 (64-bit)
+GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o ${Packages}-linux-amd64 main.go
+echo "Built monitor-service-amd64 for amd64"
+
+# Build for ARM64
+GOOS=linux GOARCH=arm64 CGO_ENABLED=0 go build -o ${Packages}-linux-arm64 main.go
+echo "Built monitor-service-arm64 for arm64"
